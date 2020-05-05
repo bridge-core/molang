4fab86b412605514d9b81c03f03e9f746ac63fcc
solvedDev <solvedDev@gmail.com>
2020-05-05 16:31:06 +0200

    remove

--- lib/Tokenizer.ts
+++
@@ -1,8 +0,0 @@
-import { Token } from "./tokens/Token";
-import { CreateToken } from "./tokens/CreateToken";
-
-export class Tokenizer {
-    static parse(str: string): Token {
-        return CreateToken.create(str.trim());
-    }
-}
