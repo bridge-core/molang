3b710f559c6a740bb5cfe8c1f3d37b2f14b7556d
solvedDev <solvedDev@gmail.com>
2020-08-13 21:41:49 +0200

    static expression optimizer

--- lib/parser/parselets/statement.ts
+++ lib/parser/parselets/statement.ts
@@ -4,19 +4,24 @@ import { TToken } from '../../tokenizer/token'
 import { IInfixParselet } from './infix'
 import { StatementExpression } from '../expressions/statement'
 import { ReturnExpression } from '../expressions/return'
+import { StaticExpression } from '../expressions/static'
 
 export class StatementParselet implements IInfixParselet {
 	constructor(public precedence = 0) {}
 
 	parse(parser: Parser, left: IExpression, token: TToken) {
 		const expr = parser.parseExpression()
-		// console.log(
-		// 	parser.useOptimizer,
-		// 	left instanceof ReturnExpression,
-		// 	left,
-		// 	expr
-		// )
-		if (parser.useOptimizer && left instanceof ReturnExpression) return left
-		return new StatementExpression(left, expr)
+		const isRightStatic = parser.useOptimizer && expr.isStatic()
+		const isLeftStatic = parser.useOptimizer && left.isStatic()
+
+		if (parser.useOptimizer && left instanceof ReturnExpression) {
+			if (isLeftStatic) return new StaticExpression(left.eval())
+			return left
+		}
+
+		return new StatementExpression(
+			left,
+			isRightStatic ? new StaticExpression(expr.eval()) : expr
+		)
 	}
 }
