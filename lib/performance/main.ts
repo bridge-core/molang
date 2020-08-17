5bb4e859d98ffc8f2318d0dab04d71edaf7efde9
solvedDev <solvedDev@gmail.com>
2020-08-17 12:24:04 +0200

    update perf tests to show raw execution time

--- lib/performance/main.ts
+++ lib/performance/main.ts
@@ -5,7 +5,7 @@ import { setEnv } from '../env'
 
 const iterations = 10000
 const expression =
-	'false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0; false ? 1 : 0;'
+	'variable.hand_bob = query.life_time < 0.01 ? 0.0 : variable.hand_bob + ((query.is_on_ground && query.is_alive ? math.clamp(math.sqrt(math.pow(query.position_delta(0), 2.0) + math.pow(query.position_delta(2), 2.0)), 0.0, 0.1) : 0.0) - variable.hand_bob) * 0.02;'
 
 const env = {
 	'variable.hand_bob': 0,
@@ -28,6 +28,11 @@ for (let i = 0; i < iterations; i++) {
 	execute(expression, true, true)
 }
 console.timeEnd('[PARSE & EXECUTE] Default Performance')
+console.time('[EXECUTE] Performance')
+for (let i = 0; i < iterations; i++) {
+	execute(expression, true, true)
+}
+console.timeEnd('[EXECUTE] Performance')
 
 console.log('-- MOLANGJS --')
 Molang.cache_enabled = false
